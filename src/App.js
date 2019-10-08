48dc436c67195ac27b4220d6191472949209fbe9
dawnlunacy <rudd.lacy@gmail.com>
2019-10-08 17:13:04 -0600

    Add initial app setup and fix sass variable path

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component }from 'react';
+// import logo from './logo.svg';
 import './App.scss';
-import './src/variables.scss'
+import './variables.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <main>
+        hi
+      </main>
+    )
+  }
 }
 
 export default App;
