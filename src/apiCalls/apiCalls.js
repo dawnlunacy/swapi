2757d8badd44c9ba9cea902b7de7dafe15c50905
Jessie Le-Ho <lehoj@oregonstate.edu>
2019-10-10 17:14:19 -0600

    Make method for fetching species url

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -2,23 +2,34 @@ export const getPeople = peopleUrl => {
     return fetch(peopleUrl)
     .then(response => response.json())
     .then(people => {
-        const peopleData = people.results.map(person => {
-            console.log('homie',person.homeworld)
-            const { name, height, mass, hair_color, skin_color, eye_color, birth_year, gender, films} = person
-            // return getHomeworld(person.homeworld).then(worlds => console.log('WORLDS AYYY',worlds))
+        const peopleData = people.results.map(person => { //"NEXT" SHOWS MORE CHARACTERS, BUT IT'S OUTSIDE OF RESULTS ON THE SAME LEVEL
+            // console.log('homie',person.homeworld)
+            const { name, films, species} = person
+            // console.log('person species', person.species)
 
-            return getHomeworld(person.homeworld).then(world => ({name, height, mass, hair_color, skin_color, eye_color, birth_year, gender, films, world}))
+            return getHomeworld(person.homeworld).then(world => ({name, films, species, world}))
+            // return getSpecies(person.species).then(species => ({name, height, species}))
         })
         return Promise.all(peopleData)
     })
+    
 }
 
 const getHomeworld = homeworldUrl => {
     return fetch(homeworldUrl)
         .then(response => response.json())
         .then(homeworlds => {
-            console.log('HOMES', homeworlds);
+            console.log('HOLMES', homeworlds);
             const { name, population } = homeworlds;
             return ({ name, population })
         })
+}
+
+const getSpecies = speciesUrl => {
+    return fetch(speciesUrl)
+            .then(species => {
+                console.log('species', species)
+                const { name, language} = species
+                return ({name, language})
+            })
 }
