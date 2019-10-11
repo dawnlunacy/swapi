e9a366912960a3d986d08b88e6336de56f4f35ba
dawnlunacy <rudd.lacy@gmail.com>
2019-10-10 20:22:41 -0600

    Clean up console.logs

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -3,18 +3,13 @@ export const getPeople = peopleUrl => {
     .then(response => response.json())
     .then(people => {
         const peopleData = people.results.map(person => { //"NEXT" SHOWS MORE CHARACTERS, BUT IT'S OUTSIDE OF RESULTS ON THE SAME LEVEL
-            // console.log('homie',person.homeworld)
             const { name, films, species, homeworld} = person
-            // console.log('person species', person.species)
 
-            // const homeWorld = getHomeworld(person.homeworld).then(world => ({name, films, species, world})).then(foo1 => console.log("foo1",foo1))
             const homeWorldInfo = getHomeworld(homeworld).then(world => world)
 
             // return homeWorld
-            // const speciesDetail =  getSpecies(person.species).then(fool => console.log("chicken", fool ))
             const speciesDetail =  getSpecies(species).then(info => info)
 
-            // return ({ name, films, species: speciesDetail, homeWorld: homeWorld })
             const personData = Promise.all([speciesDetail, homeWorldInfo]).then(data => ({ 
                 name, 
                 films, 
@@ -23,7 +18,6 @@ export const getPeople = peopleUrl => {
                 homeWorldPopulation: data[1].population
             }))
             return personData
-            // return getSpecies(person.species).then(species => ({name, height, species}))
         })
       
 
@@ -36,18 +30,15 @@ const getHomeworld = homeworldUrl => {
     return fetch(homeworldUrl)
         .then(response => response.json())
         .then(homeworlds => {
-            console.log('HOLMES', homeworlds);
             const { name, population } = homeworlds;
             return ({ name, population })
         })
 }
 
 export const getSpecies = speciesUrl => {
-    console.log("duh", speciesUrl)
     return fetch(speciesUrl)
             .then(response => response.json())
             .then(species => {
-                console.log('species', species)
                 const { name, language} = species
                 return ({name, language})
             })
