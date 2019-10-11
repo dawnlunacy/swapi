1726996c67784a5f9b8b33d975bc53078aff9886
dawnlunacy <rudd.lacy@gmail.com>
2019-10-10 20:20:57 -0600

    Win at fetching and returning persons

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -4,35 +4,28 @@ export const getPeople = peopleUrl => {
     .then(people => {
         const peopleData = people.results.map(person => { //"NEXT" SHOWS MORE CHARACTERS, BUT IT'S OUTSIDE OF RESULTS ON THE SAME LEVEL
             // console.log('homie',person.homeworld)
-            const { name, films, species} = person
+            const { name, films, species, homeworld} = person
             // console.log('person species', person.species)
 
             // const homeWorld = getHomeworld(person.homeworld).then(world => ({name, films, species, world})).then(foo1 => console.log("foo1",foo1))
-            const homeWorld = getHomeworld(person.homeworld).then(world => ({name, films, species, world})).then(foo1 => foo1)
+            const homeWorldInfo = getHomeworld(homeworld).then(world => world)
 
             // return homeWorld
             // const speciesDetail =  getSpecies(person.species).then(fool => console.log("chicken", fool ))
-            const speciesDetail =  getSpecies(person.species).then(fool => fool)
+            const speciesDetail =  getSpecies(species).then(info => info)
 
-            return ({ name, films, species: speciesDetail, homeWorld: homeWorld })
+            // return ({ name, films, species: speciesDetail, homeWorld: homeWorld })
+            const personData = Promise.all([speciesDetail, homeWorldInfo]).then(data => ({ 
+                name, 
+                films, 
+                species: data[0].name, 
+                homeWorld: data[1].name,
+                homeWorldPopulation: data[1].population
+            }))
+            return personData
             // return getSpecies(person.species).then(species => ({name, height, species}))
         })
-        console.log("HEHE", peopleData)
-        // console.log("HEHE666", [[{...peopleData[0].name}], [{...peopleData[0].films}], [{...peopleData[0].species}], [{...peopleData[0].world}]])
-
-
-        // peopleData.map(person => console.log("RES", person.homeWorld.then(hi => console.log("hi", hi))))
-        // const peopleDataResolve = peopleData.map(person => person.then(personData => personData))
-        // console.log("RESOLVE?", peopleDataResolve)
-
-        // console.log("HEHE2", peopleData.map(person => person.species).then(res1 => res1))
-
-
-        // let arrayToResolve = [...peopleData.homeWorld, ...peopleData.species]
-        // console.log("WHAT", Promise.all(arrayToResolve))
-        // .then(moooose => console.log("moooose", moooose))
-        // const answer = Promise.all([[peopleData[0].homeWorld.then(res => res)], [peopleData[1].homeWorld.then(res => res)], [peopleData[2].homeWorld.then(res => res)], [peopleData[3].homeWorld.then(res => res)]]).then(please => please)
-        console.log("ANSWER", answer)
+      
 
         return Promise.all(peopleData)
     })
