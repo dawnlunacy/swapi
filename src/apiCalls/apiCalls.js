44042180a6ebcfa43d4880f74acb0bbbcbcd5bc9
Jessie Le-Ho <lehoj@oregonstate.edu>
2019-10-10 19:59:52 -0600

    Make all fetches work

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -4,11 +4,20 @@ export const getPeople = peopleUrl => {
     .then(people => {
         const peopleData = people.results.map(person => { //"NEXT" SHOWS MORE CHARACTERS, BUT IT'S OUTSIDE OF RESULTS ON THE SAME LEVEL
             // console.log('homie',person.homeworld)
-            const { name, films, species} = person
+            const { name, films} = person
             // console.log('person species', person.species)
+            const homeworldData = getHomeworld(person.homeworld)
+            const speciesData = getSpecies(person.species)
+            // console.log('homeworld data', homeworldData)
 
-            return getHomeworld(person.homeworld).then(world => ({name, films, species, world}))
-            // return getSpecies(person.species).then(species => ({name, height, species}))
+            return Promise.all([homeworldData, speciesData]).then(([homeworlds, species])=> { //"slick way of destructuring"
+                    console.log("yo", homeworlds)
+                    // const homeworlds = fetchedData[0]
+                    // const species = fetchedData[1]
+                    return { name, films, species, homeworlds}
+                    
+                })
+
         })
         return Promise.all(peopleData)
     })
@@ -18,17 +27,18 @@ export const getPeople = peopleUrl => {
 const getHomeworld = homeworldUrl => {
     return fetch(homeworldUrl)
         .then(response => response.json())
-        .then(homeworlds => {
-            console.log('HOLMES', homeworlds);
-            const { name, population } = homeworlds;
-            return ({ name, population })
-        })
+        // .then(homeworlds => {
+        //     // console.log('HOLMES', homeworlds);
+        //     const { name, population } = homeworlds;
+        //     return ({ name, population })
+        // })
 }
 
 const getSpecies = speciesUrl => {
     return fetch(speciesUrl)
+            .then(response => response.json())
             .then(species => {
-                console.log('species', species)
+                // console.log('species', species)
                 const { name, language} = species
                 return ({name, language})
             })
