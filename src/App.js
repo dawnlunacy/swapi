040a00ce657d1ac861cb97cdf03cd77debe0cac5
dawnlunacy <rudd.lacy@gmail.com>
2019-10-11 19:04:08 -0600

    Add logic to push to collab

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component }from 'react';
 import './App.scss';
 import './variables.scss'
 
-import { getPeople, getMovies, getCharacter } from './apiCalls/apiCalls'
+import { getPeople, getMovies, getCharacters } from './apiCalls/apiCalls'
 
 import CardContainer from './CardContainer/CardContainer'
 
@@ -28,20 +28,24 @@ class App extends Component {
       })
 }
 
+// getMovieCharacters(characterUrls) {
+//   console.log("CLICK CKLICKDSK")
+//   // console.log("INTAKE",characterUrls)
+//   const firstTenCastMembers = characterUrls.splice(0, 10)
+//   console.log("10?", firstTenCastMembers)
+//   const firstTenCastInfo = firstTenCastMembers.map(url => getCharacter(url));
+//   // this.setState({currentCharacters: firstTenCastInfo})
+//   console.log("CastInfo", Promise.all(firstTenCastInfo).then(resp => console.log("AHH", resp)))
+//   // Promise.all(firstTenCastInfo).then(characterData => this.setState({currentCharacters: characterData}))
+//   // console.log("CastInfo", Promise.all(firstTenCastInfo))
+// }
+
 getMovieCharacters(characterUrls) {
   console.log("CLICK CKLICKDSK")
-  // console.log("INTAKE",characterUrls)
-  const firstTenCastMembers = characterUrls.splice(0, 10)
-  console.log("10?", firstTenCastMembers)
-  const firstTenCastInfo = firstTenCastMembers.map(url => getCharacter(url));
-  // this.setState({currentCharacters: firstTenCastInfo})
-  console.log("CastInfo", Promise.all(firstTenCastInfo).then(resp => console.log("AHH", resp)))
-  // Promise.all(firstTenCastInfo).then(characterData => this.setState({currentCharacters: characterData}))
-  // console.log("CastInfo", Promise.all(firstTenCastInfo))
+  const charactersInfo = getCharacters(characterUrls).then(resp => console.log("Character Resp:", resp))
+  // const charactersInfo = getCharacters(characterUrls)
+  // console.log("CHARACTERINFO", charactersInfo)
 
-
-
-  
 }
 
   render() {
--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -23,40 +23,77 @@
 //     })
 // }
 
-export const getCharacter = characterUrl => {
-    const array = []
-    return fetch(characterUrl)
-    .then(response => response.json())
-    .then(character => {
-        console.log("CHARACTER", character )
-        // const characterData = character.results.map(person => { //"NEXT" SHOWS MORE CHARACTERS, BUT IT'S OUTSIDE OF RESULTS ON THE SAME LEVEL
-            const { name, films, species, homeworld} = character;
-
-            const homeWorldInfo = getHomeworld(homeworld).then(world => world)
-
-        //     // return homeWorld
-            const speciesDetail =  getSpecies(species).then(info => info)
-
-            const characterData = Promise.all([speciesDetail, homeWorldInfo]).then(data => ({ 
-                name, 
-                films, 
-                species: data[0].name, 
-                homeWorld: data[1].name,
-                homeWorldPopulation: data[1].population
-            }))
-            array.push(characterData)
-
-            // return characterData
-        return Promise.all(array)
+export const getCharacters = characterUrls => {
+    const charactersInfo = [];
+     const maybe = characterUrls.map(url => {
+        fetch(url)
+        .then(response => response.json())
+        .then(person => {
+                const { name, films, species, homeworld } = person
+    
+                const homeWorldInfo = getHomeworld(homeworld).then(world => world)
+    
+                // return homeWorld
+                const speciesDetail =  getSpecies(species).then(info => info)
+    
+                const personData = Promise.all([speciesDetail, homeWorldInfo]).then(data => ({ 
+                    name, 
+                    films, 
+                    species: data[0].name, 
+                    homeWorld: data[1].name,
+                    homeWorldPopulation: data[1].population
+                }))
+                // charactersInfo.push(personData)
+                return personData
+        }).then(response => {
+            console.log("responding:", response);
+            charactersInfo.push(response);
+            console.log("infoAfter", charactersInfo)
+            // console.log("MaybeAfter", maybe)
+            return response
 
         })
-        .then(hmm => {
-            console.log("array", array.length)
-            return hmm
-        })
+    })
+        // .then(resp => console.log("inside get char:", resp))
+        // console.log("1", charactersInfo)
+    return Promise.all(maybe)
+
+}
+
+// export const getCharacter = characterUrl => {
+//     const array = []
+//     return fetch(characterUrl)
+//     .then(response => response.json())
+//     .then(character => {
+//         console.log("CHARACTER", character )
+//         // const characterData = character.results.map(person => { //"NEXT" SHOWS MORE CHARACTERS, BUT IT'S OUTSIDE OF RESULTS ON THE SAME LEVEL
+//             const { name, films, species, homeworld} = character;
+
+//             const homeWorldInfo = getHomeworld(homeworld).then(world => world)
+
+//         //     // return homeWorld
+//             const speciesDetail =  getSpecies(species).then(info => info)
+
+//             const characterData = Promise.all([speciesDetail, homeWorldInfo]).then(data => ({ 
+//                 name, 
+//                 films, 
+//                 species: data[0].name, 
+//                 homeWorld: data[1].name,
+//                 homeWorldPopulation: data[1].population
+//             }))
+//             array.push(characterData)
+
+//             // return characterData
+//         return Promise.all(array)
+
+//         })
+//         .then(hmm => {
+//             console.log("array", array.length)
+//             return hmm
+//         })
 
         // .then(characterInfo => characterInfo).then(maybe => console.log("?", maybe))
-    }
+    // }
 
 const getHomeworld = homeworldUrl => {
     return fetch(homeworldUrl)
