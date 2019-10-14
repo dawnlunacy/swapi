2bbaa724d05aa3e5328db8d033b0812d77d00ef4
dawnlunacy <rudd.lacy@gmail.com>
2019-10-13 21:21:47 -0600

    Add CharacterCard, could not get one card to work with dates

--- src/Card/CharacterCard.js
+++ src/Card/CharacterCard.js
@@ -1,17 +1,32 @@
-<div className="card">
-            <header className="card-header">
-            <h2> {data.name} </h2>
 
-            </header>
-            <main className="card-main">
-            <h3>  {data.species}</h3>
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Card.scss'
 
-            </main>
-            <footer>
-            {/* <button onClick={event => findCharacters(data.characters)}> View Characters </button> */}
-            <Link to='/movies/moose' className='btn'>  
-            <button onClick={event => findCharacters(data.characters)}> View Characters </button>            
-             </Link>
+const Card = ({ data, findCharacters}) => {
+    // console.log("CardProp",data)
+    // console.log("findCharacters", findCharacters)
 
-            </footer>
-        </div>
+    return(
+        <div className="card">
+        <header className="card-header">
+        <h2> {data.name} </h2>
+
+        </header>
+        <main className="card-main">
+        <h3>  {data.species}</h3>
+
+        </main>
+        <footer>
+        {/* <button onClick={event => findCharacters(data.characters)}> View Characters </button> */}
+        <Link to='/movies/moose' className='btn'>  
+        <button onClick={event => findCharacters(data.characters)}> View Characters </button>            
+         </Link>
+
+        </footer>
+    </div>
+
+    )
+}
+
+export default Card;
